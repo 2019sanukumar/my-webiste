7bc3348b9a89f765a490079b495e9c0d882a0e7e
Sanu Kumar <sanukumar@Sanus-MacBook-Air.local>
2021-05-15 15:47:23 +0530

    js gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,17 @@ const gulp=require('gulp');
 const sass=require('gulp-sass');
 const cssnano=require('gulp-cssnano');
 const rev=require('gulp-rev');
+const uglify =require('gulp-uglify-es').default;
 
-
-gulp.task('css',function(){
+const del=require('del');
+gulp.task('css',function(done){
     console.log('minifiny cs');
     gulp.src('./assets/css/**/*.css')
     .pipe(sass())
     .pipe(cssnano())
     .pipe(gulp.dest('./assets.css'))
 
-    return gulp.src('./assets/**/*.css')
+    gulp.src('./assets/**/*.css')
     .pipe(rev())
     .pipe(gulp.dest('./public/assets'))
     .pipe(rev.manifest({
@@ -19,4 +20,34 @@ gulp.task('css',function(){
         merge:true
     }))
     .pipe(gulp.dest('./public/assets'));
+    done();
+});
+
+gulp.task('js',function(done){
+    console.log('mann js');
+    gulp.src('./assets/**/*.js')
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(rev.manifest({
+        cwd:'public',
+        merge:true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
 })
+
+gulp.task('clean:assets',function(done)
+{
+    del.sync('./public/assets');
+    done();
+});
+
+gulp.task('build',gulp.series('clean:assets','css','js'),function(done){
+    console.log('building asstes');
+    done();
+})
+
+
+
+
+
